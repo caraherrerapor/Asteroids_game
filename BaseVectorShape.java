b2f5b5f8a8eabd04d84a625814bda3ce4bdfb5e7
carlos herrera <caraherrerapor@gmail.com>
2021-02-09 16:18:19 -0500

    Definiendo atributos y metodos en la clase BaseVectorShape

--- BaseVectorShape.java
+++ BaseVectorShape.java
@@ -1,14 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Components;
 
-/**
- *
- * @author lagra
- */
+ */package Components;
+ 
+ import java.awt.Shape;
+import java.util.Objects;*/package Components;
+ 
+ import java.awt.Shape;
+
+
 public class BaseVectorShape {
+    // atributes_ variables
+    private Shape shape;
+    private boolean  alive;
+    private  double x,y;
+    private  double velx, vely;
+    private double movAngle,faceAngle;
+    
+    //methods
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getVelx() {
+        return velx;
+    }
+
+    public double getVely() {
+        return vely;
+    }
+
+    public double getMovAngle() {
+        return movAngle;
+    }
+
+    public double getFaceAngle() {
+        return faceAngle;
+    }
+    
+    // mutator
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setVelx(double velx) {
+        this.velx = velx;
+    }
+
+    public void setVely(double vely) {
+        this.vely = vely;
+    }
+
+    public void setMovAngle(double movAngle) {
+        this.movAngle = movAngle;
+    }
+
+    public void setFaceAngle(double faceAngle) {
+        this.faceAngle = faceAngle;
+    }
+
+    
+    
+    
+    
     
 }
